0727395b6b925ea16657f93a08c21d004d65f145
Erwan Croze <erwancroze@gmail.com>
2017-11-14 23:04:47 +0100

    Update CMakelist + Add more classes

---
+++ source/engine/entity/Squad.h
@@ -0,0 +1,6 @@
+#ifndef _U_SQUAD_H
+#define _U_SQUAD_H
+
+#include "core/Types.h"
+
+#endif //_U_SQUAD_H
---
+++ source/engine/map/Cell.h
@@ -0,0 +1,7 @@
+#ifndef _U_CELL_H
+#define _U_CELL_H
+
+#include "core/Types.h"
+
+
+#endif //_U_CELL_H
